c06cace932518ece6c525a0020a9a793cf192d0a
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-07 15:05:37 -0400

    all

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const sortByNumber = function (arr) {
  *******************/
 const dieNumberOne = document.querySelector("#d6-button");
 const oneSixDie = document.querySelector("#d6-roll");
-
+//Dice one
 dieNumberOne.addEventListener("click", onClick);
 function onClick() {
   let rng = getRandomNumber(6);
@@ -58,6 +58,139 @@ function onClick() {
   }
   console.log(rng);
 }
+//================================================
+
+const dieNumberTwo = document.querySelector("#double-d6-buttons");
+const dieNumberThree = document.querySelector("#double-d6-buttons");
+const oneSixDie2 = document.querySelector("#double-d6-roll-1");
+const oneSixDie3 = document.querySelector("#double-d6-roll-2");
+
+//TwoDice==============
+
+
+dieNumberTwo.addEventListener("click", onClick2);
+
+ function onClick2() {
+  let rng = getRandomNumber(6);
+  getRandomNumber(6);
+
+  if (rng == 1) {
+    oneSixDie2.src = "images/d6/1.png"
+  } else if (rng == 2) {
+    oneSixDie2.src = "images/d6/2.png"
+  } else if (rng == 3) {
+    oneSixDie2.src = "images/d6/3.png"
+  } else if (rng == 4) {
+    oneSixDie2.src = "images/d6/4.png"
+  } else if (rng == 5) {
+    oneSixDie2.src = "images/d6/5.png"
+  } else if (rng == 6) {
+    oneSixDie2.src = "images/d6/6.png"
+  }
+  console.log(rng);
+}
+
+dieNumberThree.addEventListener("click", onClick3);
+function onClick3() {
+  let rng = getRandomNumber(6);
+  getRandomNumber(6);
+
+  if (rng == 1) {
+    oneSixDie3.src = "images/d6/1.png"
+  } else if (rng == 2) {
+    oneSixDie3.src = "images/d6/2.png"
+  } else if (rng == 3) {
+    oneSixDie3.src = "images/d6/3.png"
+  } else if (rng == 4) {
+    oneSixDie3.src = "images/d6/4.png"
+  } else if (rng == 5) {
+    oneSixDie3.src = "images/d6/5.png"
+  } else if (rng == 6) {
+    oneSixDie3.src = "images/d6/6.png"
+  }
+}
+//==========================================================
+//Dice number four
+
+const dieNumberFour = document.querySelector("#d12-button");
+const oneSixDie4 = document.querySelector("#d12-roll");
+
+dieNumberFour.addEventListener("click", onClick4);
+function onClick4() {
+  let rng = getRandomNumber(12);
+  getRandomNumber(12);
+
+  if (rng == 1) {
+    oneSixDie4.src = "images/numbers/1.png"
+  } else if (rng == 2) {
+    oneSixDie4.src = "images/numbers/2.png"
+  } else if (rng == 3) {
+    oneSixDie4.src = "images/numbers/3.png"
+  } else if (rng == 4) {
+    oneSixDie4.src = "images/numbers/4.png"
+  } else if (rng == 5) {
+    oneSixDie4.src = "images/numbers/5.png"
+  } else if (rng == 6) {
+    oneSixDie4.src = "images/numbers/6.png"
+  } else if (rng == 7) {
+    oneSixDie4.src = "images/numbers/7.png"
+  } else if (rng == 8) {
+    oneSixDie4.src = "images/numbers/8.png"
+  } else if (rng == 9) {
+    oneSixDie4.src = "images/numbers/9.png"
+  } else if (rng == 10) {
+    oneSixDie4.src = "images/numbers/10.png"
+  } else if (rng == 11) {
+    oneSixDie4.src = "images/numbers/11.png"
+  } else if (rng == 12) {
+    oneSixDie4.src = "images/numbers/12.png"
+
+  }
+  console.log(rng);
+
+  }
+//=================================================================
+
+//20 Sided dice========================
+
+const dieNumberFour = document.querySelector("#d12-button");
+const oneSixDie4 = document.querySelector("#d12-roll");
+
+dieNumberFour.addEventListener("click", onClick4);
+function onClick4() {
+  let rng = getRandomNumber(12);
+  getRandomNumber(12);
+
+  if (rng == 1) {
+    oneSixDie4.src = "images/numbers/1.png"
+  } else if (rng == 2) {
+    oneSixDie4.src = "images/numbers/2.png"
+  } else if (rng == 3) {
+    oneSixDie4.src = "images/numbers/3.png"
+  } else if (rng == 4) {
+    oneSixDie4.src = "images/numbers/4.png"
+  } else if (rng == 5) {
+    oneSixDie4.src = "images/numbers/5.png"
+  } else if (rng == 6) {
+    oneSixDie4.src = "images/numbers/6.png"
+  } else if (rng == 7) {
+    oneSixDie4.src = "images/numbers/7.png"
+  } else if (rng == 8) {
+    oneSixDie4.src = "images/numbers/8.png"
+  } else if (rng == 9) {
+    oneSixDie4.src = "images/numbers/9.png"
+  } else if (rng == 10) {
+    oneSixDie4.src = "images/numbers/10.png"
+  } else if (rng == 11) {
+    oneSixDie4.src = "images/numbers/11.png"
+  } else if (rng == 12) {
+    oneSixDie4.src = "images/numbers/12.png"
+
+  }
+  console.log(rng);
+
+  }
+
 
 /******************
  * RESET FUNCTION *
